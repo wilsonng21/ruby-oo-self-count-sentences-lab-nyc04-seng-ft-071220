4a914b93fc9f11a3eb5a9fd7e6960f777b4a3834
wilsonng21 <wilsongch92@gmail.com>
2020-08-04 20:20:09 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true 
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    array = self.split
+    total = 0
+    total += array.count { |element| element.sentence? }
+    total += array.count { |element| element.question? }
+    total += array.count { |element| element.exclamation? }
+    # binding.pry
   end
 end
